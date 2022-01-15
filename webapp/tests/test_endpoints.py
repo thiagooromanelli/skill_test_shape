38fbafe533d81e69b4bb4b487d1d91edda8b48c9
Thiago Romanelli <thiago.romanelli@tivit.com>
2022-01-15 10:49:29 -0300

    Initial commit

---
+++ webapp/tests/test_endpoints.py
@@ -0,0 +1,15 @@
+import unittest
+
+from webapp.app import app
+
+
+class AppTestCase(unittest.TestCase):
+
+    def test_ping(self):
+        tester = app.test_client(self)
+        response = tester.get('/ping')
+        assert 'PONG' in response.get_json()['message']
+
+
+if __name__ == '__main__':
+    unittest.main()
